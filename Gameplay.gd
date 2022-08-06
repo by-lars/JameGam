4ccea329b143ae97a1ad0398cb659396dd1dd339
Lars <lars@pfrenger.com>
2022-08-06 18:24:57 +0200

    asd

---
+++ Gameplay.gd
@@ -0,0 +1,4 @@
+extends Spatial
+
+func _on_Phone_button_pressed(button):
+	print(button)
--- Telephone/Buttons.gd
+++ Telephone/Buttons.gd
@@ -1,5 +1,7 @@
 extends Spatial
 
+signal button_pressed(button)
+
 onready var keys = [$"RootNode/Key007", $"RootNode/Key", $"RootNode/Key004", $"RootNode/Key008", $"RootNode/Key001", $"RootNode/Key005", $"RootNode/Key009", $"RootNode/Key002", $"RootNode/Key006", $"RootNode/Key010", $"RootNode/Key003", $"RootNode/Key011"]
 
 func _ready():
@@ -20,5 +22,6 @@ func _on_Keypad_input_event(camera, event, position, normal, shape_idx, extra_ar
 		var obj = keys[extra_arg_0]
 		if obj.get_meta("pressedState") == 0:
 			obj.set_meta("pressedState", 1)
+			emit_signal("button_pressed", extra_arg_0)
 			$Tween.interpolate_property(obj, "global_transform:origin", obj.global_transform.origin, obj.global_transform.origin - (Vector3.UP * 0.05), 0.4, Tween.TRANS_BACK, Tween.EASE_IN_OUT)		
 			$Tween.start()
