9486f93f462e60a2674e34a97abef535f69ede21
Lars <lars@pfrenger.com>
2022-08-06 18:20:21 +0200

    buttons now work for real

--- Telephone/Buttons.gd
+++ Telephone/Buttons.gd
@@ -1,29 +1,24 @@
 extends Spatial
 
-
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
-
 onready var keys = [$"RootNode/Key007", $"RootNode/Key", $"RootNode/Key004", $"RootNode/Key008", $"RootNode/Key001", $"RootNode/Key005", $"RootNode/Key009", $"RootNode/Key002", $"RootNode/Key006", $"RootNode/Key010", $"RootNode/Key003", $"RootNode/Key011"]
 
-# Called when the node enters the scene tree for the first time.
 func _ready():
-	print("rdy")
-	pass # Replace with function body.
+	for key in keys:
+		key.set_meta("pressedState", 0)
+	$Tween.connect("tween_completed", self, "on_tween_completed")
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
-
-func tween_completed ( Object object, NodePath key ):
-	pass
-
+func on_tween_completed(obj, key):
+	if obj.get_meta("pressedState") == 1:
+		obj.set_meta("pressedState", 2)
+		$Tween.interpolate_property(obj, "global_transform:origin", obj.global_transform.origin, obj.global_transform.origin + (Vector3.UP * 0.05), 0.2, Tween.TRANS_BACK, Tween.EASE_IN_OUT)		
+		$Tween.start()
+	else:
+		obj.set_meta("pressedState", 0)
 
 func _on_Keypad_input_event(camera, event, position, normal, shape_idx, extra_arg_0):
 	if event is InputEventMouseButton:
 		var obj = keys[extra_arg_0]
-		$Tween.interpolate_property(obj, "global_transform:origin", obj.global_transform.origin, obj.global_transform.origin - (Vector3.UP * 0.05), 0.5, Tween.TRANS_BACK, Tween.EASE_IN_OUT)		
-		$Tween.start()
-		print("press")
+		if obj.get_meta("pressedState") == 0:
+			obj.set_meta("pressedState", 1)
+			$Tween.interpolate_property(obj, "global_transform:origin", obj.global_transform.origin, obj.global_transform.origin - (Vector3.UP * 0.05), 0.4, Tween.TRANS_BACK, Tween.EASE_IN_OUT)		
+			$Tween.start()
